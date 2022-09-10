8d93d82999d5883e9d162e01e41d4a020aa58656
Shikhar10081 <111962558+Shikhar10081@users.noreply.github.com>
2022-09-10 21:20:19 +0530

    6

---
+++ 19(6).py
@@ -0,0 +1,13 @@
+def big(a,b,c,d):
+    if a>b and a>c and a>d:
+        print(a)
+    else:
+        if b>c and b>d:
+            print(b)
+        else:
+            if c>d:
+                print(c) 
+            else:
+                print(d)      
+
+big(10,2,77,9)
